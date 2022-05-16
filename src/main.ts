078008d49165e23e07a661a6c0d80be8789d8382
Firdo.dev <83067641+firdodev@users.noreply.github.com>
2022-05-16 16:54:20 +0200

    Added Navigation Mesh

--- src/main.ts
+++ src/main.ts
@@ -1,48 +1,192 @@
-import "@babylonjs/core/Debug/debugLayer";
-import "@babylonjs/inspector";
-import "@babylonjs/loaders/glTF";
-import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder } from "@babylonjs/core";
-import {MainGame} from './MainGame';
+var canvas = document.getElementById("renderCanvas");
 
+var startRenderLoop = function (engine, canvas) {
+    engine.runRenderLoop(function () {
+        if (sceneToRender && sceneToRender.activeCamera) {
+            sceneToRender.render();
+        }
+    });
+}
 
-class App {
-    constructor() {
-        // create the canvas html element and attach it to the webpage
-        var canvas = document.createElement("canvas");``
-        canvas.style.width = "100%";
-        canvas.style.height = "100%";
-        canvas.id = "gameCanvas";
-        document.body.appendChild(canvas);
+var engine = null;
+var scene = null;
+var sceneToRender = null;
+var createDefaultEngine = function() { return new BABYLON.Engine(this.canvas, true, { preserveDrawingBuffer: true, stencil: true,  disableWebGL2Support: false}); };
+var agents = [];
 
-        // initialize babylon scene and engine
-        var engine = new Engine(canvas, true);
-        var scene = new Scene(engine);
+var createScene = function () {
 
-        var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
-        camera.attachControl(canvas, true);
-        var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
+    // This creates a basic Babylon Scene object (non-mesh)
+    var scene = new BABYLON.Scene(engine);
+    let navigationPlugin = new BABYLON.RecastJSPlugin();
+    navigationPlugin.setWorkerURL("./src/workers/navMeshWorker.js");
+
+    // This creates and positions a free camera (non-mesh)
+    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(-6, 4, -8), scene);
+    // This targets the camera to scene origin
+    camera.setTarget(BABYLON.Vector3.Zero());
+    // This attaches the camera to the canvas
+    // camera.attachControl(canvas, true);
+    camera.detachControl();
+
+    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+    // Default intensity is 1. Let's dim the light a small amount
+    light.intensity = 0.7;
+
+    var staticMesh = createStaticMesh(scene);
+    var navmeshParameters = {
+        cs: 0.2,
+        ch: 0.2,
+        walkableSlopeAngle: 90,
+        walkableHeight: 1.0,
+        walkableClimb: 1,
+        walkableRadius: 1,
+        maxEdgeLen: 12.,
+        maxSimplificationError: 1.3,
+        minRegionArea: 8,
+        mergeRegionArea: 20,
+        maxVertsPerPoly: 6,
+        detailSampleDist: 6,
+        detailSampleMaxError: 1,
+        };
+
+    navigationPlugin.createNavMesh([staticMesh], navmeshParameters,(navmeshData) =>
+    {
+        console.log("got worker data", navmeshData);
+        navigationPlugin.buildFromNavmeshData(navmeshData);
+        var navmeshdebug = navigationPlugin.createDebugNavMesh(scene);
+        navmeshdebug.position = new BABYLON.Vector3(0, 0.01, 0);
+
+        var matdebug = new BABYLON.StandardMaterial('matdebug', scene);
+        matdebug.diffuseColor = new BABYLON.Color3(0.1, 0.2, 1);
+        matdebug.alpha = 0.2;
+        navmeshdebug.material = matdebug;
         
-        new MainGame(scene);
+        // crowd
+        var crowd = navigationPlugin.createCrowd(10, 0.1, scene);
+        var i;
+        var agentParams = {
+            radius: 0.1,
+            height: 0.2,
+            maxAcceleration: 4.0,
+            maxSpeed: 1.0,
+            collisionQueryRange: 0.5,
+            pathOptimizationRange: 0.0,
+            separationWeight: 1.0};
+            
+        for (i = 0; i <1; i++) {
+            var width = 0.20;
+            //player
+            var agentCube = BABYLON.MeshBuilder.CreateBox("cube", { size: width, height: width }, scene);
+            
+            var targetCube = BABYLON.MeshBuilder.CreateBox("cube", { size: 0.1, height: 0.1 }, scene);
+            var matAgent = new BABYLON.StandardMaterial('mat2', scene);
+            var variation = Math.random();
+            matAgent.diffuseColor = new BABYLON.Color3(0.4 + variation * 0.6, 0.3, 1.0 - variation * 0.3);
+            agentCube.material = matAgent;
+            var randomPos = navigationPlugin.getRandomPointAround(new BABYLON.Vector3(-2.0, 0.1, -1.8), 0.5);
+            var transform = new BABYLON.TransformNode("transform");
+            //agentCube.parent = transform;
+            var agentIndex = crowd.addAgent(randomPos, agentParams, transform);
+            agents.push({idx:agentIndex, trf:transform, mesh:agentCube, target:targetCube});
+        }
+        
+        var startingPoint;
+        var currentMesh;
+        var pathLine;
+        var getGroundPosition = function () {
+            var pickinfo = scene.pick(scene.pointerX, scene.pointerY);
+            if (pickinfo.hit) {
+                return pickinfo.pickedPoint;
+            }
 
+            return null;
+        }
 
-        // hide/show the Inspector
-        window.addEventListener("keydown", (ev) => {
-            // Shift+Ctrl+Alt+I
-            if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.keyCode === 73) {
-                if (scene.debugLayer.isVisible()) {
-                    scene.debugLayer.hide();
-                } else {
-                    scene.debugLayer.show();
+        var pointerDown = function (mesh) {
+                currentMesh = mesh;
+                startingPoint = getGroundPosition();
+                if (startingPoint) { // we need to disconnect camera from canvas
+                    setTimeout(function () {
+                        // camera.detachControl(this.canvas);
+                    }, 0);
+                    var agents = crowd.getAgents();
+                    var i;
+                    for (i=0;i<agents.length;i++) {
+                        var randomPos = navigationPlugin.getRandomPointAround(startingPoint, 1.0);
+                        crowd.agentGoto(agents[i], navigationPlugin.getClosestPoint(startingPoint));
+                    }
+                    var pathPoints = navigationPlugin.computePath(crowd.getAgentPosition(agents[0]), navigationPlugin.getClosestPoint(startingPoint));
+                    pathLine = BABYLON.MeshBuilder.CreateDashedLines("ribbon", {points: pathPoints, updatable: true, instance: pathLine}, scene);
+                }
+        }
+        
+        scene.onPointerObservable.add((pointerInfo) => {      		
+            switch (pointerInfo.type) {
+                case BABYLON.PointerEventTypes.POINTERDOWN:
+                    if(pointerInfo.pickInfo.hit) {
+                        pointerDown(pointerInfo.pickInfo.pickedMesh)
+                    }
+                    break;
+                    }
+                    });
+
+        scene.onBeforeRenderObservable.add(()=> {
+            var agentCount = agents.length;
+            for(let i = 0;i<agentCount;i++)
+            {
+                var ag = agents[i];
+                ag.mesh.position = crowd.getAgentPosition(ag.idx);
+                let vel = crowd.getAgentVelocity(ag.idx);
+                crowd.getAgentNextTargetPathToRef(ag.idx, ag.target.position);
+                if (vel.length() > 0.2)
+                {
+                    vel.normalize();
+                    var desiredRotation = Math.atan2(vel.x, vel.z);
+                    ag.mesh.rotation.y = ag.mesh.rotation.y + (desiredRotation - ag.mesh.rotation.y) * 0.05;
                 }
             }
         });
+    }); // worker
+    return scene;
+};
 
-        // run the main render loop
-        engine.runRenderLoop(() => {
-            scene.render();
-            engine.resize();
-        });
-    }
+function createStaticMesh(scene) {
+    var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
+
+    // Materials
+    var mat1 = new BABYLON.StandardMaterial('mat1', scene);
+    mat1.diffuseColor = new BABYLON.Color3(1, 1, 1);
+    //cube.material = mat2;
+
+    var mesh = BABYLON.Mesh.MergeMeshes([ ground]);
+    return mesh;
 }
-new App();
 
+var initFunction = async function() {
+            
+    await Recast();
+    // console.log("Recast .......... ",Recast);
+    var asyncEngineCreation = async function() {
+        try {
+            return createDefaultEngine();
+        } catch(e) {
+            console.log("the available createEngine function failed. Creating the default engine instead");
+            return createDefaultEngine();
+        }
+    }
+
+    window.engine = await asyncEngineCreation();
+    if (!engine) throw 'engine should not be null.';
+    startRenderLoop(engine, canvas);
+    window.scene = createScene();
+};
+
+initFunction().then(() => {sceneToRender = scene                    
+});
+
+// Resize
+window.addEventListener("resize", function () {
+    engine.resize();
+});
